b708f5ea7b567b2c9a66381d18999a0ae2baae31
Alvin <alvin@dsaid.gov.sg>
2023-04-12 17:42:37 +0800

    wrap auth item

--- src/plugins/otp-jwt-auth/wrap-components/wrapAuthItem.jsx
+++ src/plugins/otp-jwt-auth/wrap-components/wrapAuthItem.jsx
@@ -1,26 +1,28 @@
 import React from "react"
 
-const wrapAuthItem = (Ori, system) => function WrappedAuthItem({ Ori, ...props }) {
+const wrapAuthItem = (Ori, system) => function WrappedOtpJwtAuthItem({ Ori, ...props }) {
   const {
-    schema, getComponent, authSelectors, errSelectors, authorized, onAuthChange, name
+    schema, getComponent, errSelectors, authorized, onAuthChange, name
   } = props
 
   const OtpJwtAuth = getComponent("OtpJwtAuth")
   const type = schema.get("type")
   const scheme = schema.get("scheme")
   const otp = schema.get("otp")
-
-  if(type === "http" && scheme === 'bearer' && otp) {
+  
+  if(type === "http" && scheme === 'bearer' && !!otp) {
     return <OtpJwtAuth key={ name }
               schema={ schema }
               name={ name }
-              authSelectors={authSelectors}
+              authSelectors={system.getSystem().authSelectors}
               errSelectors={ errSelectors }
               authorized={ authorized }
               getComponent={ getComponent }
               onChange={ onAuthChange }
               getSystem={system.getSystem} />
-  } else {
+  }
+  else if(!Ori) return <div />
+  else {
     return <Ori {...props} />
   }
 }
--- src/plugins/saml-auth/wrap-components/wrapAuthItem.jsx
+++ src/plugins/saml-auth/wrap-components/wrapAuthItem.jsx
@@ -1,25 +1,28 @@
 import React from "react"
 
-const wrapAuthItem = (Ori, system) => function WrappedAuthItem({ Ori, ...props }) {
+const wrapAuthItem = (Ori, system) => function WrappedSamlAuthItem({ Ori, ...props }) {
   const {
-    schema, getComponent, authSelectors, errSelectors, authorized, onAuthChange, name
+    schema, getComponent, errSelectors, authorized, onAuthChange, name
   } = props
 
-  const SamlAuthItem = getComponent("SamlAuthItem")
+  const SamlAuth = getComponent("SamlAuth")
   const type = schema.get("type")
   const scheme = schema.get("scheme")
+  const saml = schema.get("saml")
 
-  if(type === "http" && scheme == 'bearer') {
-    return <SamlAuthItem key={ name }
+  if(type === "http" && scheme == 'bearer' && !!saml) {
+    return <SamlAuth key={ name }
               schema={ schema }
               name={ name }
-              authSelectors={authSelectors}
+              authSelectors={system.getSystem().authSelectors}
               errSelectors={ errSelectors }
               authorized={ authorized }
               getComponent={ getComponent }
               onChange={ onAuthChange }
               getSystem={system.getSystem} />
-  } else {
+  }
+  else if(!Ori) return <div />
+  else {
     return <Ori {...props} />
   }
 }
