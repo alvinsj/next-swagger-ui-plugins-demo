ef8a2ee95e4d3355f88baf0b11652220a8552e89
Alvin <alvin@dsaid.gov.sg>
2023-04-12 17:41:11 +0800

    add mock apis

---
+++ src/pages/api/auth/otp/tokens.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.setHeader('Content-Type', 'application/json').send({token: 'secret-token!'});
+}
---
+++ src/pages/api/service/authorized-only.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if(req.headers.authorization)
+    res.json({hello: 'world!'});
+  else
+    res.status(401).json({message: 'Unauthorized!'});
+}
