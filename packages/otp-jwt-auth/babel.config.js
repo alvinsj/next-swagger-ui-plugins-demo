1681cd76f977d39881c341ecc49130f88aac3266
Alvin <alvin@dsaid.gov.sg>
2023-05-03 17:23:40 +0800

    add packages

--- packages/otp-jwt-auth/babel.config.js
+++ packages/otp-jwt-auth/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   plugins: ["@babel/plugin-transform-modules-commonjs"],
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  presets: ["@babel/preset-react", ['@babel/preset-env', {targets: {node: 'current'}}]],
 };
--- packages/otp-jwt-auth/jest.config.js
+++ packages/otp-jwt-auth/jest.config.js
@@ -1,4 +1,3 @@
-/** @type {import('jest').Config} */
 const config = {
   verbose: true,
   transformIgnorePatterns: [
