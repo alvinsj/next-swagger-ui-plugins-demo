e9f24fc3470ad360abad50f270cb0cbddf61a0b1
Alvin <alvin@dsaid.gov.sg>
2023-05-04 14:52:23 +0800

    add test

---
+++ packages/otp-jwt-auth/wrap-components/__tests__/wrapAuthItem.test.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import wrapAuthItem from "../wrapAuthItem"
+import renderer from 'react-test-renderer'
+import { Map } from 'immutable'
+
+describe('wrapAuthItem', () => {
+  const system = {
+    getSystem: () => system,
+    otpJwtAuthSelectors: {}, 
+    authSelectors: {},
+    errSelectors: {},
+    getComponent: (name) => function MockComponent ({children, ...props})  {
+      return (
+        <mock-component id={name} data-props={props}>
+        {children}
+        </mock-component>
+      )
+    }
+  }
+  const Ori = () => <mock-element id={'Ori'} />
+  const WrappedComponent = wrapAuthItem(Ori, system)
+
+  it('returns null component for others', () => {
+    expect(WrappedComponent).toBeInstanceOf(Function)
+    
+    // match snapshot
+    const tree = renderer
+      .create(
+        <WrappedComponent
+          schema={new Map()}
+          name="OtpJwtAuth"
+          {...system}
+        />) 
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('returns an OtpJwtAuth component', () => {
+    expect(WrappedComponent).toBeInstanceOf(Function)
+    
+    // match snapshot
+    const tree = renderer
+      .create(
+        <WrappedComponent
+          schema={new Map({type: 'http', scheme: 'bearer', otp: true})}
+          name="OtpJwtAuth"
+          {...system}
+        />) 
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
